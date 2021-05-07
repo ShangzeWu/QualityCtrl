08bb40901752391cf247d2991d8f68af76f64629
ShangzeWu <56289293+ShangzeWu@users.noreply.github.com>
2021-05-07 09:43:54 +0800

    Update upload_file_B.php

--- No1FileTool/upload_file_B.php
+++ No1FileTool/upload_file_B.php
@@ -1,12 +1,11 @@
 <?php
 
-$allowedExts = array("xls", "xlsx");
+$allowedExts = array("xlsx");
 $temp = explode(".", $_FILES["file"]["name"]);  //以.作为分隔符
 echo $_FILES["file"]["size"]."kb"."<br>";
 echo "请注意，文件大小不能超过2000kb"."<br>";
 $extension = end($temp);     // 获取文件后缀名
-if ((($extension == "xls")
-|| ($extension == "xlsx"))
+if (($extension == "xlsx")
 && ($_FILES["file"]["size"] < 2048000)   // 小于 2000 kb
 && in_array($extension, $allowedExts))
 {
@@ -39,7 +38,8 @@ header("Location: http://47.114.178.105/QualityCtrl/No1FileTool/preForB.php");
 }
 else
 {
-    echo "文件过大或非法的文件格式,仅能上传xlsx或xls，请尽量上传xlsx"."<br>";
+    echo "上传失败，错误信息：";
+    echo "文件过大或非法的文件格式,仅能上传xlsx，不支持xls文件"."<br>";
     echo "如需上传更大的文件，请联系管理员";
 }
 
