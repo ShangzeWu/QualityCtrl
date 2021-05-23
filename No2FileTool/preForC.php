5424a1043a9865e72dc0a6f3b9dfa6f1700dcd86
ShangzeWu <56289293+ShangzeWu@users.noreply.github.com>
2021-05-23 20:45:48 +0800

    Update preForC.php

--- No2FileTool/preForC.php
+++ No2FileTool/preForC.php
@@ -5,12 +5,12 @@ header("content-type:text/html;charset=utf-8");
 exec("python3 /var/www/html/QualityCtrl/No2FileTool/preForC.py 2>&1",$out,$ret);
 print_r($out);
 print_r($ret);
-exec("python3 /var/www/html/QualityCtrl/No2FileTool/rmvoid.py 2>&1",$out1,$ret1);
-print_r($out1);
-print_r($ret1);
-exec("python3 /var/www/html/QualityCtrl/No2FileTool/CtoTemp.py 2>&1",$out2,$ret2);
-print_r($out2);
-print_r($ret2);
+#exec("python3 /var/www/html/QualityCtrl/No2FileTool/rmvoid.py 2>&1",$out1,$ret1);
+#print_r($out1);
+#print_r($ret1);
+#exec("python3 /var/www/html/QualityCtrl/No2FileTool/CtoTemp.py 2>&1",$out2,$ret2);
+#print_r($out2);
+#print_r($ret2);
 
 
 #$shell="python3 preForA.py";
